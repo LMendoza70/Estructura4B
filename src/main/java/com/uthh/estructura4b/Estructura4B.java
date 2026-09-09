ab2006fbda89d064f98814d9aeebc18258058ae4
MTI.LuisAlbertoMendoza <luis.mendoza@uthh.edu.mx>
2026-09-09 15:19:23 -0600

    Prueva de segundo comit

--- src/main/java/com/uthh/estructura4b/Estructura4B.java
+++ src/main/java/com/uthh/estructura4b/Estructura4B.java
@@ -14,6 +14,7 @@ public class Estructura4B {
 
     public static void main(String[] args) {
         cuentaBancaria cuenta=new cuentaBancaria("Luis Alberto Mendoza", "54708565325241", 15000.50f);
+        cuenta.setNumCuneta("hola esto es un ejemplo");
         
         System.out.println("Banco mendoza");
         System.out.println("Cuenta bancaria de luis");
